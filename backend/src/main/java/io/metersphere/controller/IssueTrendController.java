b0f9354ee94e4a9e85ebfb1e5914505c1c711b59
0780 <shenlei@mudu.tv>
2022-02-25 16:11:35 +0800

    Signed-off-by: 0780 <shenlei@mudu.tv> 修改打包失败

--- backend/src/main/java/io/metersphere/controller/IssueTrendController.java
+++ backend/src/main/java/io/metersphere/controller/IssueTrendController.java
@@ -40,9 +40,9 @@ public class IssueTrendController {
     @GetMapping("/issue/total/projectquality/list")
     public ResultHolder issueTrendTotal(@RequestParam HashMap<String, String> hashMap) throws HttpProcessException, ExecutionException, InterruptedException, ParseException {
         List<Map<String,Object>> result = issueTrendStatisticsService.getIssueTrendTotal(hashMap);
-        //        if (issueTrendStatisticsService.getIssueTrendTotal(hashMap).size()==0){
-//            return ResultHolder.selfInface(1,"fail","请检查环境或者个人令牌权限",issueTrendStatisticsService.getIssueTrendTotal(hashMap).size());
-//        }
+        if (result.size()==0){
+            return ResultHolder.selfInface(1,"fail","请检查环境或者个人令牌权限",0);
+        }
         return ResultHolder.selfInface(0,"success",result,result.size());
     }
 //    @RequestMapping(value = "/queryStmp", method = RequestMethod.GET)
