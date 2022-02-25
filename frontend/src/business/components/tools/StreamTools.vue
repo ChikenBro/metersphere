aa7718edb63c081ef7b3f4912442e65defcbe3fd
0780 <shenlei@mudu.tv>
2022-02-25 18:14:40 +0800

    Signed-off-by: 0780 <shenlei@mudu.tv> 修改侵短打包问题

---
+++ frontend/src/business/components/tools/StreamTools.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{name}}
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "StreamTools.vue",
+        data() {
+            return{
+                name: "hello"
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
