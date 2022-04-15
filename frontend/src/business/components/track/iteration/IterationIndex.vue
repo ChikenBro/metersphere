4e59956976751c2a964510f9b58f8a22378944db
coderzrf <602484813@qq.com>
2022-04-15 13:43:16 +0800

    fix: 同步coding迭代后重新拉取列表

--- frontend/src/business/components/track/iteration/IterationIndex.vue
+++ frontend/src/business/components/track/iteration/IterationIndex.vue
@@ -222,6 +222,7 @@ export default {
     syncIterationCoding() {
       syncCoding(() => {
         this.$success("开始同步");
+        this.fetchIterationList();
       });
     },
     showRequirementList(row) {
