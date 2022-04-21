4ee17d42187bb2fbbdb9eb4fc8662e14c9b33144
coderzrf <602484813@qq.com>
2022-04-21 10:00:06 +0800

    fix: 迭代管理当前状态标签颜色

--- frontend/src/business/components/track/iteration/IterationIndex.vue
+++ frontend/src/business/components/track/iteration/IterationIndex.vue
@@ -63,7 +63,7 @@
               :fields-width="fieldsWidth"
             >
               <template v-slot:default="scope">
-                <el-tag :type="StatusTagMap[scope.row.status].type" size="mini">
+                <el-tag :type="StatusTagMap[scope.row.status]" size="mini">
                   {{ IterationStatusEnum[scope.row.status] || "-" }}
                 </el-tag>
               </template>
