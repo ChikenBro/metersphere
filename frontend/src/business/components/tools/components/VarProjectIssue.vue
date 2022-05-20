488ef77de9f70c40ffb6d543a606eae126a077f1
coderzrf <602484813@qq.com>
2022-05-20 17:23:36 +0800

    feat:  优化各项目bug统计

--- frontend/src/business/components/tools/components/VarProjectIssue.vue
+++ frontend/src/business/components/tools/components/VarProjectIssue.vue
@@ -7,58 +7,70 @@
         :form="form"
         @onSearch="getIssueList"
       />
-      <el-card style="height: 500px; overflow: auto; margin-bottom: 10px">
-        <el-row v-if="pieOptions.length > 0">
-          <el-col v-for="(option, index) in pieOptions" :key="index" :span="6">
-            <ms-chart
-              style="width: 100%"
-              :options="option"
-              :autoresize="false"
-            />
-          </el-col>
-        </el-row>
-        <div v-else class="empty">
-          <el-empty description="暂无数据" :image-size="200" />
-        </div>
-      </el-card>
-      <el-card>
-        <el-table
-          row-key="id"
-          border
-          class="adjust-table"
-          :data="tableData"
-          :default-sort="{ prop: 'allIssue', order: 'descending' }"
-          height="calc(100vh - 150px)"
-        >
-          <el-table-column
-            prop="displayName"
-            label="项目名称"
-            show-overflow-tooltip
-          />
-          <el-table-column
-            prop="allIssue"
-            label="所有问题"
-            show-overflow-tooltip
-            sortable
-          />
-          <el-table-column
-            prop="allUnresolvedIssue"
-            label="未解决问题"
-            show-overflow-tooltip
-            sortable
-          />
-          <el-table-column
-            prop="unresolvedIssuePercent"
-            label="未解决问题率"
-            show-overflow-tooltip
-            sortable
-          >
-            <template v-slot:default="scope">
-              {{ scope.row.unresolvedIssuePercent + "%" }}
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-card>
+      <!-- 父元素必须用el-row包裹，否则页面会卡死 -->
+      <el-row>
+        <el-col :span="24">
+          <el-tabs type="border-card">
+            <el-tab-pane label="可视化图表">
+              <div v-if="pieOptions.length > 0">
+                <el-row>
+                  <el-col
+                    v-for="(option, index) in pieOptions"
+                    :key="index"
+                    :span="6"
+                  >
+                    <ms-chart
+                      style="width: 100%"
+                      :options="option"
+                      :autoresize="true"
+                    />
+                  </el-col>
+                </el-row>
+              </div>
+              <div v-else class="empty">
+                <el-empty description="暂无数据" :image-size="200" />
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="详细数据">
+              <el-table
+                row-key="id"
+                border
+                class="adjust-table"
+                :data="tableData"
+                :default-sort="{ prop: 'allIssue', order: 'descending' }"
+              >
+                <el-table-column
+                  prop="displayName"
+                  label="项目名称"
+                  show-overflow-tooltip
+                />
+                <el-table-column
+                  prop="allIssue"
+                  label="所有问题"
+                  show-overflow-tooltip
+                  sortable
+                />
+                <el-table-column
+                  prop="allUnresolvedIssue"
+                  label="未解决问题"
+                  show-overflow-tooltip
+                  sortable
+                />
+                <el-table-column
+                  prop="unresolvedIssuePercent"
+                  label="未解决问题率"
+                  show-overflow-tooltip
+                  sortable
+                >
+                  <template v-slot:default="scope">
+                    {{ scope.row.unresolvedIssuePercent + "%" }}
+                  </template>
+                </el-table-column>
+              </el-table>
+            </el-tab-pane>
+          </el-tabs>
+        </el-col>
+      </el-row>
     </el-card>
   </div>
 </template>
@@ -66,30 +78,24 @@
 <script>
 import MsChart from "@/business/components/common/chart/MsChart";
 import IssueFormHeader from "../common/IssueFormHeader";
-
+import { deepClone } from "@/common/js/tableUtils";
 const templateOption = {
   color: ["#41F73C", "#D43030"],
   title: {
     text: "标题",
     x: "center",
-    y: "10%",
+    y: "12%",
   },
   tooltip: {
     trigger: "item",
     formatter: "{b}({c}) {d}%",
   },
-  // legend: {
-  //   orient: "vertical",
-  //   left: "left",
-  //   data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"],
-  // },
-
   series: [
     {
       name: "各项目Bug统计",
       type: "pie",
-      radius: "45%",
-      center: ["50%", "55%"],
+      radius: "65%",
+      center: ["50%", "60%"],
       data: [],
       // stillShowZeroSum: false,
       itemStyle: {
@@ -97,6 +103,21 @@ const templateOption = {
           shadowBlur: 10,
           shadowOffsetX: 0,
           shadowColor: "rgba(0, 0, 0, 0.5)",
+        },
+      },
+      label: {
+        normal: {
+          position: "inner",
+          formatter(params) {
+            const { name, value, percent } = params;
+            if (value === 0 && name !== "所有问题") return "";
+            return `${name}:${value}\n(${percent}%)`;
+          },
+          textStyle: {
+            color: "#fff",
+            fontSize: 12,
+            align: "center",
+          },
         },
       },
     },
@@ -115,6 +136,7 @@ export default {
       headerComps: [],
       pieOptions: [],
       isLoading: true,
+      activeIndex: "pie",
     };
   },
   computed: {
@@ -167,17 +189,28 @@ export default {
     },
     setPieOption() {
       const pieOptions = [];
-      this.tableData.forEach((item) => {
-        let option = JSON.parse(JSON.stringify(templateOption));
+      const tableData = deepClone(this.tableData).sort(
+        (a, b) => b.allIssue - a.allIssue
+      );
+      tableData.forEach((item) => {
+        let option = deepClone(templateOption);
         option.title.text = item.displayName;
-        option.series[0].data.push({
-          name: "已解决问题",
-          value: item.allIssue - item.allUnresolvedIssue,
-        });
-        option.series[0].data.push({
-          name: "未解决问题",
-          value: item.allUnresolvedIssue,
-        });
+        if (item.allIssue !== 0) {
+          option.series[0].data.push({
+            name: "已解决问题",
+            value: item.allIssue - item.allUnresolvedIssue,
+          });
+          option.series[0].data.push({
+            name: "未解决问题",
+            value: item.allUnresolvedIssue,
+          });
+        } else {
+          option.series[0].data.push({
+            name: "所有问题",
+            value: 0,
+          });
+          option.color = ["#ccc"];
+        }
         pieOptions.push(option);
       });
       this.pieOptions = pieOptions;
@@ -204,7 +237,8 @@ export default {
 .adjust-table >>> .el-table__row {
   height: 46.2px;
 }
-.adjust-table >>> .el-table__body-wrapper {
-  height: calc(100% - 47px) !important;
+
+.issue-wrapper >>> .el-tabs__content {
+  min-height: calc(100vh - 200px);
 }
 </style>
